bde901a867b931344d519b7eccd51f334c298cbe
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-12-17 14:20:17 +0100

    Creata prima variabile (somma ) con php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $x = 10;
+        $y = 20;
+        $z = $x + $y 
+    ?>
+
+
+    <h1>HelloWorld  <?php echo "la somma tra ". $x . " e " . $y . " è " . $z ?> </h1>
+</body>
+</html>
